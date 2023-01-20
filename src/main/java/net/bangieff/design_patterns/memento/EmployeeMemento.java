d90f3f1576a14cd5bc2a4b1917140506c15ea5b8
Martin Bangiev <bangieff@gmail.com>
2023-01-20 09:00:50 -0500

    Memento pattern

---
+++ src/main/java/net/bangieff/design_patterns/memento/EmployeeMemento.java
@@ -0,0 +1,23 @@
+package net.bangieff.design_patterns.memento;
+
+//memento
+public class EmployeeMemento {
+	private String name;
+	//for the example we don't care about the address
+	private String phone;
+	
+	public EmployeeMemento(String name, String phone) {
+		this.name = name;
+		this.phone = phone;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+	
+	
+}
---
+++ src/main/java/net/bangieff/design_patterns/memento/MementoApp.java
@@ -0,0 +1,30 @@
+package net.bangieff.design_patterns.memento;
+
+public class MementoApp {
+
+	public static void main(String[] args) {
+		Caretaker caretaker = new Caretaker();
+		
+		Employee emp = new Employee();
+		emp.setName("John Doe");
+		emp.setAddress("42, Sesame Street");
+		emp.setPhone("123-456-7890");
+		caretaker.save(emp);	
+		System.out.println("employee before save:\t" + emp);
+		
+		emp.setPhone("111-222-3333");
+		caretaker.save(emp);
+		System.out.println("employee after change1:\t" + emp);
+
+		emp.setPhone("111-222-0000"); //we haven't saved this one
+		System.out.println("employee after change2:\t" + emp);
+		
+		caretaker.revert(emp);
+		System.out.println("employee after revert1:\t" + emp);
+		
+		caretaker.revert(emp);
+		System.out.println("employee after revert2:\t" + emp);
+
+	}
+
+}
